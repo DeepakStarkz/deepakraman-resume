84bb97dd83600ae4c2b3d0a4bbc64c673a4b7cc8
Deepak V R <jarvisver2@JarvisMacPC.local>
2025-08-22 23:14:08 +0530

    Add Resume contents

--- src/constants/contents.js
+++ src/constants/contents.js
@@ -1,4 +1,36 @@
-export default EXPERINECE = [];
+export const EXPERIENCE = [
+  {
+    title: 'IT Analyst',
+    location: 'Chennai',
+    company: 'Tata Consultancy Services',
+    period: 'August 2024 - Present',
+    responsibilities: [
+      'Collaborated with project teams on implementation initiatives such as upgrades or migrations from legacy systems.',
+      'Performed troubleshooting activities to identify root cause of system issues and implement corrective actions.',
+      'Provided ongoing support for software applications including upgrades, patches, and bug fixes.',
+      'Participation in on-call rota for 24x7 application support.',
+      'Integrated the E2E Applepay testing with browserstack automated real device testing.',
+      'Migrated the observability from Newrelic to Dynatrace.',
+    ],
+  },
+  {
+    title: 'System Engineer',
+    location: 'Chennai',
+    company: 'Tata Consultancy Services',
+    period: 'January 2020 - August 2024',
+    responsibilities: [
+      'Responsible for frontend design and development of E-commerce webpage.',
+      'Integrated new payments such as Google Pay and Sparks Pay that improved the user experience on the checkout page.',
+      'Improved existing payment integration such as Apple Pay, which increased the user conversion rate by 4%.',
+      'Optimized Paypal integration and improved the conversion rate by 3%.',
+      'Integrated the CICD pipeline with Jenkins for the pull request build which improved coding standards.',
+      'Integrated New Relic with the page to improve observability, which helped find and fix bugs, improving overall end-user experience.',
+      'Migrating the payment page to the new framework which is based on Next.js, React and Typescript.',
+      'Coached and mentored team members to improve capabilities.',
+    ],
+  },
+];
+
 export const SKILLS = [
   'JavaScript',
   'TypeScript',
@@ -14,14 +46,14 @@ export const SKILLS = [
 export const EDUCATION = [
   {
     degree: 'Bachelor of Engineering in Mechanical Engineering',
-    institution: 'XYZ University',
+    institution: 'C.Abdul Hakeem College of Engineering and Technology',
     year: '2015 - 2019',
     cgpa: '7.95/10',
   },
 ];
 export const CONTACT = {
-  email: 'deepakstarkz4@gmail.com',
-  phone: '+91 8072597900',
+  email: 'deepakxxxxx@gmail.com',
+  phone: '+91 8072xxxxx',
   socialMedia: {
     linkedin: 'https://www.linkedin.com/in/deepak-vr1111/',
     github: 'https://github.com/DeepakStarkz',
@@ -33,13 +65,13 @@ export const PROJECTS = [
   {
     title: 'Portfolio Website',
     description:
-      'A personal portfolio website to showcase my projects and skills.',
-    link: 'https://deepakraman.github.io',
+      'A minimalistic portfolio website to showcase my skills, experience, and projects.',
+    link: 'https://deepakstarkz.github.io/deepakraman-resume/',
   },
   {
-    title: 'E-commerce Platform',
+    title: 'LeetCode Profile',
     description:
-      'A full-stack e-commerce application built with React and Node.js.',
-    link: '',
+      'A collection of my coding challenges and solutions on LeetCode.',
+    link: 'https://leetcode.com/u/Dpak_Wayne/',
   },
 ];
